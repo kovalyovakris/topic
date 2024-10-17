406dfca5586cc19e1082895b0baed83830789430
kovayovakris <kristtinakovalyova23@gmail.com>
2024-10-17 11:00:32 +0300

    да ххуй знает

--- тема2/obj/Debug/net8.0-windows/тема2.AssemblyInfo.cs
+++ тема2/obj/Debug/net8.0-windows/тема2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("тема2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a33612c4b6ad09ae9035dfc0cdb33f80de298a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5303c48d00f90bb17299a3f49fe7fe931ce33c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("тема2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("тема2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
